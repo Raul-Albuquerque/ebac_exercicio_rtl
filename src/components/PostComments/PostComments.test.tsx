903ba4de54f07c54d6f9f73e281456fdf2c9017a
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-11-07 15:39:17 -0300

    inclusão de testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,26 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment/>)
         expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
-});
+    })
+    test('Deve incluir 2 comentários na publicação', () => {
+        render(<PostComment />)
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Muito legal'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Gostei demais'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        expect(screen.getAllByTestId('comentario')).toHaveLength(2)
+    })
+})
